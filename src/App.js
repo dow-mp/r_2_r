9893ab946844b0e74efcc7a9cd5d20d20e776be5
dow-mp <derin.mitchell@gmail.com>
2022-06-15 21:38:32 -0400

    pass props to list component and initiate handleChange function for search input field

--- src/App.js
+++ src/App.js
@@ -1,33 +1,34 @@
 import * as React from 'react';
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-  {
-    title: 'MongoDB',
-    url: 'https://mongo.db',
-    author: 'Someone',
-    num_comments: 5,
-    points: 3,
-    objectID: 2,
-  },
-];
+const App = () => {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+    {
+      title: 'MongoDB',
+      url: 'https://mongo.db',
+      author: 'Someone',
+      num_comments: 5,
+      points: 3,
+      objectID: 2,
+    },
+  ];
 
-const App = () => (
+  return (
     <div>
       <h1>My Hacker Stories</h1>
 
@@ -36,17 +37,19 @@ const App = () => (
       <hr />
       
       {/*utilize the List component within the App component*/}
-      <List />
+      <List list={stories}/>
 
       {/* creating another instance of list element - practicing component instantiation */}
       {/* <List /> */}
 
     </div>
-  );
+  )
+};
 
-const List = () => (
+// pass in the props to the List component - we always pass in "props" because there may be several attributes or props passed into the component from the parent and we can access them by stating props._____ (the name of the attribute)
+const List = (props) => (
     <ul>
-      {list.map((item) => (
+      {props.list.map((item) => (
           <li key={item.objectID}>
             <span>
               <a href={item.url}>{item.title}</a>
@@ -60,11 +63,28 @@ const List = () => (
     </ul>
   );
 
-const Search = () => (
+const Search = () => {
+  const handleChange = (e) => {
+    // console.log(e) logs EVERY letter that gets added to the input field (i.e. every change made to the field)
+    // console.log(e)
+    
+    // console.log(e.target.value) drills down to only log the specific character entered into the input field 
+    //it continues to log each letter as a change event and therefore the console log looks like:
+    // p
+    // py
+    // pyt
+    // pyth
+    // pytho
+    // python
+    console.log(e.target.value);
+  }
+
+  return (
     <div>
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+      <input id="search" type="text" onChange={handleChange} />
     </div>
-  );
+  )
+};
 
 export default App;
