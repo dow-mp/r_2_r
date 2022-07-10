0da1d86ef933def39e1412d04243eb3a62e0a6a5
dow-mp <derin.mitchell@gmail.com>
2022-07-10 11:24:34 -0400

    lift state, initiate destructuring props object for more concise use of props

--- src/App.js
+++ src/App.js
@@ -61,26 +61,44 @@ const App = () => {
 };
 
 // pass in the props to the List component - we always pass in "props" because there may be several attributes or props passed into the component from the parent and we can access them by stating props._____ (the name of the attribute)
-const List = (props) => {
+const List = ({ list }) => {
   console.log('List renders');
   return (
     <ul>
-      {props.list.map((item) => (
-          <li key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-          </li>
+      {/* can create another component called Item to further simplify the list component and keep each function separate */}
+
+      {/* could use rest operator here in the signature of the .map() function as such:  .map(({objectId, ...item}) => etc. etc.)    but this is not as readable for a beginner dev (any maybe not even for another dev unfamiliar with the project) */}
+      {list.map((item) => ( 
+          <Item // could use spread operator here: <Item key={objectId} {...item}   but this comes at a cost of readability
+            // instead, have passed in all properties of the item object as props here and will use below in the <Item /> component as destructured properties that appear in the function siganature of <Item />
+            key={item.objectId}
+            title={item.title}
+            url={item.url}
+            author={item.author}
+            num_comments={item.num_comments}
+            points={item.points}
+          />
         )
       )}
     </ul>
   )
 };
 
-const Search = (props) => {
+const Item = ({title, url, author, num_comments, points}) => {
+  return (
+    // In the book, R2R, the key is not used here, but only passed in as props in the List component above - so is using the key in the list item here repetitive? since each item will have received it above?
+    <li>
+      <span>
+        <a href={url}>{title}</a>
+      </span>
+      <span>{author}</span>
+      <span>{num_comments}</span>
+      <span>{points}</span>
+    </li>
+  )
+}
+
+const Search = ({search, onSearch}) => {
   //console.log('Search renders');
 
   return (
@@ -89,8 +107,8 @@ const Search = (props) => {
       <input 
         id="search" 
         type="text"
-        value={props.search}
-        onChange={props.onSearch}
+        value={search}
+        onChange={onSearch}
       />
     </div>
   );
