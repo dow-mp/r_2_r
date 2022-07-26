95ad9cee55bd1f209f00553da8f0eb3b21051c5e
dow-mp <derin.mitchell@gmail.com>
2022-07-26 00:08:37 -0400

    reusable components, imperative react for autofocus, and inline handlers in jsx

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
 // import * as React from 'react';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
+
+const initialStories = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+  {
+    title: 'MongoDB',
+    url: 'https://mongo.db',
+    author: 'Someone',
+    num_comments: 5,
+    points: 3,
+    objectID: 2,
+  },
+];
 
 // initiate building custom hook to encompass functionality of useState and useEffect hooks
 // use generic parameters (instead of searchTerm/setSearchTerm) so that this hook can be resused as needed throughout the application
@@ -7,42 +34,13 @@ const useSemiPersistentState = (key, initialState) => {
   const [value, setValue] = useState(
     localStorage.getItem(key) || initialState
   );
-
   useEffect(() => {
     localStorage.setItem(key, value);
   }, [value, key]);
-
   return [value, setValue];
 };
 
 const App = () => {
-  //console.log('App renders');
-  const stories = [
-    {
-      title: 'React',
-      url: 'https://reactjs.org',
-      author: 'Jordan Walke',
-      num_comments: 3,
-      points: 4,
-      objectID: 0,
-    },
-    {
-      title: 'Redux',
-      url: 'https://redux.js.org',
-      author: 'Dan Abramov, Andrew Clark',
-      num_comments: 2,
-      points: 5,
-      objectID: 1,
-    },
-    {
-      title: 'MongoDB',
-      url: 'https://mongo.db',
-      author: 'Someone',
-      num_comments: 5,
-      points: 3,
-      objectID: 2,
-    },
-  ];
 
   // previous use of useState and useEffect hooks prior to building custom hook
     /* const [searchTerm, setSearchTerm] = useState(
@@ -55,8 +53,13 @@ const App = () => {
 
   const [searchTerm, setSearchTerm] = useSemiPersistentState(
     'search',
-    'React'
+    ''
   );
+
+  // initiate stateful stories array
+  const [stories, setStories] = useState(initialStories);
+
+
 
   // add a callback handler function to App component to handle what happens when Search component renders - this function will be passed into Search component as props
   const handleSearch = (e) => {
@@ -69,17 +72,43 @@ const App = () => {
     return story.title.toLowerCase().includes(searchTerm.toLowerCase());
   });
 
+  // create callback handler to remove a specific searched story
+  const handleRemoveStory = (item) => {
+    const newStories = stories.filter(
+      (story) => item.objectID !== story.objectID
+    );
+    setStories(newStories);
+  };
+
+
+
   return (
     <>
       <h1>My Hacker Stories</h1>
 
       {/* passing in the handleSearch callback handler function as props to the Search component */}
-      <Search onSearch={handleSearch} search={searchTerm}/>
+      <InputWithLabel
+        id="search"
+        value={searchTerm}
+        isFocused
+        onInputChange={handleSearch}
+      >
+        <strong>Search: </strong>
+      </InputWithLabel>
+
+      {/* testing autofocus feature with two input/label boxes */}
+      <InputWithLabel
+        id="input"
+        value={searchTerm}
+        onInputChange={handleSearch}
+      >
+        <strong>Input: </strong>
+      </InputWithLabel>
 
       <hr />
       
       {/*utilize the List component within the App component*/}
-      <List list={searchedStories}/>
+      <List list={searchedStories} onRemoveItem={handleRemoveStory}/>
 
       {/* creating another instance of list element - practicing component instantiation */}
       {/* <List /> */}
@@ -88,8 +117,7 @@ const App = () => {
 };
 
 // pass in the props to the List component - we always pass in "props" because there may be several attributes or props passed into the component from the parent and we can access them by stating props._____ (the name of the attribute)
-const List = ({ list }) => {
-  console.log('List renders');
+const List = ({ list, onRemoveItem }) => {
   return (
     <ul>
       {/* can create another component called Item to further simplify the list component and keep each function separate */}
@@ -104,6 +132,8 @@ const List = ({ list }) => {
             author={item.author}
             num_comments={item.num_comments}
             points={item.points}
+            item={item}
+            onRemoveItem={onRemoveItem}
           />
         )
       )}
@@ -111,9 +141,14 @@ const List = ({ list }) => {
   )
 };
 
-const Item = ({title, url, author, num_comments, points}) => {
+const Item = ({title, url, author, num_comments, points, item, onRemoveItem}) => {
+  // creating another handler function to call the function passed in as props from List which was passed down to list from App (this is a "normal handler")
+  const handleRemoveItem = () => {
+    // in the book R2R, Robin passes 'item' from List by mapping several <Item> components per each item passed into the map function; but if I passed down each item's attributes separately as in {title, url, author, num_comments, points} how can I then pass these arguments to the onRemoveItem function for it to remove the affected HTML elements from view on the page??
+    onRemoveItem(title, url, author, num_comments, points);
+  }
+
   return (
-    // In the book, R2R, the key is not used here, but only passed in as props in the List component above - so is using the key in the list item here repetitive? since each item will have received it above?
     <li>
       <span>
         <a href={url}>{title}</a>
@@ -121,23 +156,42 @@ const Item = ({title, url, author, num_comments, points}) => {
       <span>{author}</span>
       <span>{num_comments}</span>
       <span>{points}</span>
+      <span>
+        {/* passing the handler declared above to the button as a function that should execute onClick */}
+        {/* <button type="button" onClick={handleRemoveItem}> */}
+        {/* another option - create in inline handler which binds arguments to the function for execution of the function with those given arguments */}
+        <button type="button" onClick={onRemoveItem.bind(null, title, url, author, num_comments, points)}>
+        {/* option 3 - an alternative inline handler function using an anonymous function to allow us to utilize the function with an argument */}
+        {/* <button type="button" onClick={() => onRemoveItem(item)}> */}
+          Dismiss
+        </button>
+      </span>
     </li>
   )
 }
 
-const Search = ({search, onSearch}) => {
-  //console.log('Search renders');
+const InputWithLabel = ({id, type='text', children, value, onInputChange, isFocused}) => {
+  const inputRef = useRef();
+
+  useEffect(() => {
+    if(isFocused && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [isFocused]);
 
   return (
-    <div>
-      <label htmlFor="search">Search: </label>
+    <>
+      <label htmlFor={id}>{children}</label>
+      &nbsp;
       <input 
-        id="search" 
-        type="text"
-        value={search}
-        onChange={onSearch}
+        ref={inputRef}
+        id={id} 
+        type={type}
+        value={value}
+        autoFocus={isFocused}
+        onChange={onInputChange}
       />
-    </div>
+    </>
   );
 };
 
