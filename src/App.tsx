b17d6d9e26b492eb9652dfd2fbcf8cea9c6401b1
dow-mp <derin.mitchell@gmail.com>
2022-10-03 15:13:12 -0400

    add buttons to toggle previous 5 searches

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
-// import * as React from 'react';
 import { useState, useEffect, useRef, useReducer, useCallback } from 'react';
 import axios from 'axios';
-// import styles from './App.module.css';
 import styled from 'styled-components';
-// // importing check mark svg for use as button "text" to dismiss articles
 import { ReactComponent as Check } from './check.svg';
 import { sortBy } from 'lodash';
 
-
-// defining a Story type for use in the Item component in order to keep code DRY
 type Story = {
   objectID: string;
   title?: string;
@@ -21,19 +16,16 @@ type Story = {
 
 type Stories = Array<Story>;
 
-// we can even simplify things further by typing the ItemProps as below
 type ItemProps = {
   item: Story;
   onRemoveItem: (item: Story) => void;
 };
 
-// pre-defining the types for props passed into the List component
 type ListProps = {
   list: Stories;
   onRemoveItem: (item: Story) => void;
 };
 
-// adding types for states, actions below
 type StoriesState = {
   data: Stories;
   isLoading: boolean;
@@ -82,8 +74,6 @@ type SearchFormProps = {
   onSearchSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
 }
 
-
-// defining styled-components
 const StyledContainer = styled.div`
   height: fit-content;
   width: 100vw;
@@ -91,6 +81,11 @@ const StyledContainer = styled.div`
   background: #83a4d4;
   background: linear-gradient(to left, #b6fbff, #83a4d4);
   color: #171212;
+`;
+
+const StyledContainerSearch = styled.div`
+  display: inline-block;
+  padding: 10px 5px;
 `;
 
 const StyledHeadlinePrimary = styled.h1`
@@ -114,7 +109,6 @@ const StyledColumn = styled.span`
   a {
     color: inherit;
   }
-
 `;
 
 const StyledColumnTitle = styled.span`
@@ -205,7 +199,6 @@ const StyledButton = styled.button`
 
 const StyledButtonSmall = styled(StyledButton)`
   padding: 5px;
-  // adding styling for when the button is hovered over
   &:hover {
     background-color: hotpink;
   }
@@ -245,7 +238,6 @@ const StyledInput = styled.input`
 
 // actions for the useReducer dispatch functions which will instruct what type of action to perform based on the reducer function below - storiesReducer
 const ACTIONS = {
-  // SET_STORIES: "SET_STORIES",
   REMOVE_STORY: "REMOVE_STORY",
   STORIES_FETCH_INIT: "STORIES_FETCH_INIT",
   STORIES_FETCH_FAILURE: "STORIES_FETCH_FAILURE",
@@ -254,29 +246,22 @@ const ACTIONS = {
 
 const API_ENDPOINT = 'http://hn.algolia.com/api/v1/search?query=';
 
-// initiate building custom hook to encompass functionality of useState and useEffect hooks
-// use generic parameters (instead of searchTerm/setSearchTerm) so that this hook can be resused as needed throughout the application
+// initiate building re-useable custom hook to encompass functionality of useState and useEffect hooks
 const useSemiPersistentState = (
-  // add type safety for the arguments key and initialState using TypeScript
   key: string,
   initialState: string
-  // below corresponds to the return value at the bottom of this hook; telling typescript that this function returns an [array] containing a string (the state value) and that the setState function returns nothing (void)
 ): [string, (newValue: string) => void] => {
-  // we do not need to add additional type safety features below because of React and Typescript working together with type inference (i.e. if the initial state is a string, then the returned current state will be returned as a string and the updater function will only accept a string and return nothing)
   const [value, setValue] = useState(
-    // value is inferred to be a string
-    // setValue only takes a string as an argument 2' type inference
     localStorage.getItem(key) || initialState
   );
-  // when the state of the value changes, re-setting the local storage to this new value using a side effect
+
   useEffect(() => {
       localStorage.setItem(key, value);
     }, [value, key]);
   return [value, setValue];
 };
 
-// reducer function for the useReducer hook
-// this function takes in the current state and an action THEN based on the type of action the following instructions are carried out
+// reducer function for the useReducer hook - takes in the current state and an action THEN based on the type of action the following instructions are carried out
 const storiesReducer = (
   state: StoriesState,
   action: any,
@@ -314,6 +299,17 @@ const storiesReducer = (
   }
 };
 
+
+const extractSearchTerm = (url : string) => {
+  return url.replace(API_ENDPOINT, '');
+}
+
+const getLastSearches = (urls : Array<string>) => {
+  return urls.slice(-6).slice(0, -1).map(extractSearchTerm);
+}
+
+const getUrl = (searchTerm : string) => `${API_ENDPOINT}${searchTerm}`;
+
 const App = () => {
 
   // using the custom hook created outside of the App component to store the searchTerm to local storage when it changes as well as supply the initial state of the searchTerm input field on visit to the site
@@ -322,9 +318,8 @@ const App = () => {
     ''
   );
 
-  const [url, setUrl] = useState(`${API_ENDPOINT}${searchTerm}`);
+  const [urls, setUrls] = useState([`${API_ENDPOINT}${searchTerm}`,]);
 
-  // previously isLoading and isError were their own stateful variables, but here we merge them into useReducer hook to limit the chance of achieving an impossible state where data "isLoading" but the api returned an error - and therefore data could not possibly be loading
   const [stories, dispatchStories] = useReducer(
     storiesReducer, 
     { data: [],
@@ -334,91 +329,80 @@ const App = () => {
 
   // memoizing the callback handler function with useCallback hook - remove all fetching data logic from side effect below into its own stand alone function within the component
   const handleFetchStories = useCallback( async () => {
-    // if searchTerm does not exist, do nothing
-    // if(!searchTerm) return;
-    // dispatch the action noted to the storiesReducer function using the dispatch function - this returns state with changes to isLoading and isError as defined in the storiesReducer function above
     dispatchStories({ type: ACTIONS.STORIES_FETCH_INIT });
 
-    // use the searchTerm appended to the end of the URL query to filter results on the client side
     try {
-      const result = await axios.get(url)
-        // this dispatch function is returned from the useReducer hook and it sets the state based on the action.type - whose logic is carried out in the reducer function (if action is type: x, do z) ...in this instance, when data is returned from the promise, update the state of stories variable to include the payload and change isLoading and isError booleans as defined in the storiesReducer function
-        console.log(result.data.hits);
+      const lastUrl = urls[urls.length - 1];
+      const result = await axios.get(lastUrl)
       dispatchStories({type: ACTIONS.STORIES_FETCH_SUCCESS, payload: result.data.hits,});
     } catch {
       console.log(Error);
       dispatchStories({ type: ACTIONS.STORIES_FETCH_FAILURE }); // dispatch the failure action type to the storiesReducer function to return state changes as defined in the function
     }
-  }, [url]);
-  
-    // searchTerm in the dependecy above changes when the user enters input into the input field, this causes the function handleFetchStories to re-run and thus the side effect below runs because the function has been re-defined
+  }, [urls]);
 
-
+  // re-fetch the data from the API if the function changes (i.e. if a new search term results in the the API being pinged for hits again)
   useEffect(() => {
     handleFetchStories();
-  }, [handleFetchStories]); // this side effect is now dependent on changes to the callback function
+  }, [handleFetchStories]);
 
-  // add a callback handler function to App component to handle what happens when Search component renders - this function will be passed into Search component as props
   const handleSearchInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(e.target.value);
-    // set local storage of 'search' item to be the event's target value (or the input field value on submit)
-    // localStorage.setItem('search', e.target.value);
   };
+
+  const handleSearch = (searchTerm : string) => {
+    const url = getUrl(searchTerm);
+    setUrls(urls.concat(url));
+  }
 
   const handleSearchSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    setUrl(`${API_ENDPOINT}${searchTerm}`);
+    return handleSearch(searchTerm);
   };
 
-  // eliminiating this variable and avoiding passing it as props allows us to switch from filtering our results on the client side to filtering the results on the server side using a query directly to the API
-  // const searchedStories = stories.data.filter((story) => story.title.toLowerCase().includes(searchTerm.toLowerCase()));
-
-  // create callback handler to remove a specific searched story
-  // implement useCallback to allow memo functionality in the list component below (to create an equality check for the list component to avoid unncecessary re-renders - we need useCallback here to avoid creating a new version of this handler on each render of the app component which would then result in a new version of the list component since this is passed in as a prop)
   const handleRemoveStory = useCallback((item: Story) => {
-    // the following logic was relocated to the reducer function for use with useReducer hook for an action.type of 'remove_story'
-
-    // const newStories = stories.filter(
-    //   (story) => item.objectID !== story.objectID
-    // );
-
-    // dispatch the remove story action to the storiesReducer to run the logic above that was relocated to the reducer function in order create a new state of "data" that does not include stories where the ID matched the param ID (i.e. they are removed from the returned array value)
     dispatchStories({
       type: ACTIONS.REMOVE_STORY,
       payload: item,
     });
   }, []);
 
+  const handleLastSearch = (searchTerm : string) => {
+    return handleSearch(searchTerm);
+  };
+
+  const lastSearches = getLastSearches(urls);
+
   return (
-    // <div className="container">
-    // implement css module (imports the css stylesheet as a module or Object which you can access different properties of the object using dot notaiton)
-    // <div className={styles.container}>
-    // implement styled-components (css directly inside of js)
     <StyledContainer>
-      {/* <h1 className="headline-primary">My Hacker Stories</h1> */}
-      {/* implement css module */}
-      {/* <h1 className={styles.headlinePrimary}>My Hacker Stories</h1> */}
-      <StyledHeadlinePrimary>My Hacker Stories</StyledHeadlinePrimary>
-      <SearchForm
-        searchTerm={searchTerm}
-        onSearchInput={handleSearchInput}
-        onSearchSubmit={handleSearchSubmit}
-      />
-
-      <hr />
-      
-      {stories.isError && <p>Something went wrong...</p>}
-
-      { stories.isLoading ? <p>Loading....</p> : 
-        <List 
-          list={stories.data} 
-          onRemoveItem={handleRemoveStory}
+      <>
+        <StyledHeadlinePrimary>My Hacker Stories</StyledHeadlinePrimary>
+        <SearchForm
+          searchTerm={searchTerm}
+          onSearchInput={handleSearchInput}
+          onSearchSubmit={handleSearchSubmit}
         />
-      }
+        {lastSearches.map((searchTerm, index) => (
+          <StyledContainerSearch>
+            <StyledButtonLarge 
+              key={searchTerm + index}
+              type="button"
+              onClick={() => handleLastSearch(searchTerm)}
+              >
+                {searchTerm}
+            </StyledButtonLarge>
+          </StyledContainerSearch>
+        ))}
 
-      {/* creating another instance of list element - practicing component instantiation */}
-      {/* <List /> */}
-    {/* </div> */}
+        <hr />
+        {stories.isError && <p>Something went wrong...</p>}
+        { stories.isLoading ? <p>Loading....</p> : 
+          <List 
+            list={stories.data} 
+            onRemoveItem={handleRemoveStory}
+          />
+        }
+      </>
     </StyledContainer>
   )
 };
@@ -452,8 +436,6 @@ const List = ({list, onRemoveItem}: ListProps) => {
   const handleSort = (sortKey: string) => {
     // comparing the sortKey param with the sortKey in state (sort.sortKey) to determine if they are the same (if same, then it should reverse - but only if not already reversed)
     const isReverse = sort.sortKey === sortKey && !sort.isReverse;
-    // setSort({ sortKey: sortKey, isReverse: isReverse });
-    // implement shorthand object initializer notation
     setSort({ sortKey: sortKey, isReverse: isReverse });
   };
 
@@ -506,32 +488,7 @@ const List = ({list, onRemoveItem}: ListProps) => {
   )
 };
 
-const Item = ({item, onRemoveItem
-  // typing he passed in props as ItemProps (which are pre-typed above this component)
-}: ItemProps
-// {
-  // typing below is not DRY code because it has been duplicated, instead we can define a type outside of this component and then use it here
-  // item: {
-  //   objectID: string;
-  //   title: string;
-  //   url: string;
-  //   author: string;
-  //   num_comments: number;
-  //   points: number;
-  // };
-  // onRemoveItem: (item: {
-  //   objectID: string;
-  //   title: string;
-  //   url: string;
-  //   author: string;
-  //   num_comments: number;
-  //   points: number;
-  //   }) => void;  
-
-  // utilizing the pre-defined type Story here to keep code DRY; and then simplifying even further by defining ItemProps outside of this component and instituting above
-  // item: Story;
-  // onRemoveItem: (item: Story) => void;
-) => {
+const Item = ({ item, onRemoveItem }: ItemProps) => {
   // creating another handler function to call the function passed in as props from List which was passed down to list from App (this is a "normal handler")
   //const handleRemoveItem = () => {
     // in the book R2R, Robin passes 'item' from List by mapping several <Item> components per each item passed into the map function; but if I passed down each item's attributes separately as in {title, url, author, num_comments, points} how can I then pass these arguments to the onRemoveItem function for it to remove the affected HTML elements from view on the page??
@@ -602,43 +559,6 @@ const Item = ({item, onRemoveItem
   )
 }
 
-// class InputWithLabel extends Component {
-//   constructor(props) {
-//     super(props);
-
-//     this.inputRef = createRef();
-//   }
-
-//   componentDidMount() {
-//     if(this.props.isFocused) {
-//       this.inputRef?.current.focus();
-//     }
-//   }
-
-//   render() {
-//     const {
-//       id,
-//       value,
-//       type="text",
-//       onInputChange, 
-//       children
-//     } = this.props;
-//      return (
-//       <>
-//         <label htmlFor={id}>{children}</label>
-//         &nbsp;
-//         <input
-//           ref={this.inputRef}
-//           id={id}
-//           type={type}
-//           value={value}
-//           onChange={onInputChange}
-//         />
-//       </>
-//      );
-//   }
-// }
-
 const InputWithLabel = ({
   id,
   type='text',
@@ -657,13 +577,8 @@ const InputWithLabel = ({
 
   return (
     <>
-      {/* <label htmlFor={id} className="label">{children}</label> */}
-      {/* <label htmlFor={id} className={styles.label}>{children}</label> */}
-      {/* implement styled label component */}
       <StyledLabel htmlFor={id}>{children}</StyledLabel>
       &nbsp;
-      {/* <input  */}
-      {/* implement styled input component */}
       <StyledInput
         ref={inputRef}
         id={id} 
@@ -671,8 +586,6 @@ const InputWithLabel = ({
         value={value}
         autoFocus={isFocused}
         onChange={onInputChange}
-        // className="input"
-        // className={styles.input}
       />
     </>
   );
@@ -684,9 +597,6 @@ const SearchForm = ({
   onSearchSubmit 
 }: SearchFormProps ) => {
   return (
-    // <form onSubmit={onSearchSubmit} className="search-form">      
-    // <form onSubmit={onSearchSubmit} className={styles.searchForm}> 
-    // implement styled search form component
     <StyledSearchForm onSubmit={onSearchSubmit}>
       <InputWithLabel
         id="search"
@@ -697,18 +607,12 @@ const SearchForm = ({
         <strong>Search: </strong>
       </InputWithLabel>
 
-      {/* <button */}
-      {/* implement large styled button */}
       <StyledButtonLarge
         type="submit"
         disabled={!searchTerm}
-        // className="button button-large"
-        // className={`${styles.button} ${styles.buttonLarge}`}
       >
         Submit
-      {/* </button> */}
       </StyledButtonLarge>
-    {/* </form> */}
     </StyledSearchForm>
   )
 }
